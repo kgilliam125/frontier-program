39197322313d0dfb4b0acfc29d879348f38b5147
Kyle Gilliam <kgilliam125@gmail.com>
2023-08-11 13:24:34 -0500

    Prettify

--- tests/clash-of-clones.test.ts
+++ tests/clash-of-clones.test.ts
@@ -1,34 +1,34 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { ClashOfClones } from "../target/types/clash_of_clones";
+import * as anchor from '@coral-xyz/anchor'
+import { Program } from '@coral-xyz/anchor'
+import { ClashOfClones } from '../target/types/clash_of_clones'
 
-describe("clash-of-clones", () => {
-  // Configure the client to use the local cluster.
-  const provider = anchor.AnchorProvider.env();
-  anchor.setProvider(provider);
+describe('clash-of-clones', () => {
+    // Configure the client to use the local cluster.
+    const provider = anchor.AnchorProvider.env()
+    anchor.setProvider(provider)
 
-  const program = anchor.workspace.ClashOfClones as Program<ClashOfClones>;
+    const program = anchor.workspace.ClashOfClones as Program<ClashOfClones>
 
-  const [playerPda] = anchor.web3.PublicKey.findProgramAddressSync(
-    [Buffer.from("player"), provider.publicKey.toBuffer()],
-    program.programId
-  );
-  const [basePda] = anchor.web3.PublicKey.findProgramAddressSync(
-    [Buffer.from("base"), provider.publicKey.toBuffer()],
-    program.programId
-  );
+    const [playerPda] = anchor.web3.PublicKey.findProgramAddressSync(
+        [Buffer.from('player'), provider.publicKey.toBuffer()],
+        program.programId
+    )
+    const [basePda] = anchor.web3.PublicKey.findProgramAddressSync(
+        [Buffer.from('base'), provider.publicKey.toBuffer()],
+        program.programId
+    )
 
-  it("It initialized a player!", async () => {
-    const tx = await program.methods
-    .initPlayer()
-    .accounts({
-      playerAccount: playerPda,
-      baseAccount: basePda,
+    it('It initialized a player!', async () => {
+        const tx = await program.methods
+            .initPlayer()
+            .accounts({
+                playerAccount: playerPda,
+                baseAccount: basePda,
+            })
+            .rpc()
+
+        const playerAccount = await program.account.player.fetch(playerPda)
+
+        expect(playerAccount.ownerPubkey).toEqual(provider.publicKey)
     })
-    .rpc();
-
-    const playerAccount = await program.account.player.fetch(playerPda)
-
-    expect(playerAccount.ownerPubkey).toEqual(provider.publicKey)
-  });
-});
+})
