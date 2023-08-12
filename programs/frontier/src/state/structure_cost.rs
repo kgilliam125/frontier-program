c086c8c41787eb4b3dac1e753bcae6831aa5d5fa
Kyle Gilliam <kgilliam125@gmail.com>
2023-08-11 22:40:56 -0500

    Use actual resource types

--- programs/frontier/src/state/structure_cost.rs
+++ programs/frontier/src/state/structure_cost.rs
@@ -27,46 +27,46 @@ impl StructureCost {
         self.is_initialized = true;
         self.version = Self::VERSION;
         self.manor = [
-            Resources { wood: 0, stone: 0, iron: 0, food: 0, gold: 0 },
-            Resources { wood: 0, stone: 0, iron: 0, food: 0, gold: 1000 },
-            Resources { wood: 0, stone: 0, iron: 0, food: 0, gold: 2000 },
-            Resources { wood: 0, stone: 0, iron: 0, food: 0, gold: 4000 },
-            Resources { wood: 0, stone: 0, iron: 0, food: 0, gold: 8000 },
+            Resources { wood: 0, stone: 0, iron: 0, steel: 0, mana: 0, gold: 0 },
+            Resources { wood: 0, stone: 0, iron: 0, steel: 0, mana: 0, gold: 1000 },
+            Resources { wood: 0, stone: 0, iron: 0, steel: 0, mana: 0, gold: 2000 },
+            Resources { wood: 0, stone: 0, iron: 0, steel: 0, mana: 0, gold: 4000 },
+            Resources { wood: 0, stone: 0, iron: 0, steel: 0, mana: 0, gold: 8000 },
         ];
         self.lumber_mill = [
-            Resources { wood: 0, stone: 0, iron: 0, food: 0, gold: 500 },
-            Resources { wood: 0, stone: 0, iron: 0, food: 0, gold: 800 },
-            Resources { wood: 0, stone: 0, iron: 0, food: 0, gold: 1500 },
-            Resources { wood: 0, stone: 0, iron: 0, food: 0, gold: 2500 },
-            Resources { wood: 0, stone: 0, iron: 0, food: 0, gold: 4000 },
+            Resources { wood: 0, stone: 0, iron: 0, steel: 0, mana: 0, gold: 500 },
+            Resources { wood: 0, stone: 0, iron: 0, steel: 0, mana: 0, gold: 800 },
+            Resources { wood: 0, stone: 0, iron: 0, steel: 0, mana: 0, gold: 1500 },
+            Resources { wood: 0, stone: 0, iron: 0, steel: 0, mana: 0, gold: 2500 },
+            Resources { wood: 0, stone: 0, iron: 0, steel: 0, mana: 0, gold: 4000 },
         ];
         self.quarry = [
-            Resources { wood: 0, stone: 0, iron: 0, food: 0, gold: 600 },
-            Resources { wood: 0, stone: 0, iron: 0, food: 0, gold: 1000 },
-            Resources { wood: 0, stone: 0, iron: 0, food: 0, gold: 1800 },
-            Resources { wood: 0, stone: 0, iron: 0, food: 0, gold: 3200 },
-            Resources { wood: 0, stone: 0, iron: 0, food: 0, gold: 5000 },
+            Resources { wood: 0, stone: 0, iron: 0, steel: 0, mana: 0, gold: 600 },
+            Resources { wood: 0, stone: 0, iron: 0, steel: 0, mana: 0, gold: 1000 },
+            Resources { wood: 0, stone: 0, iron: 0, steel: 0, mana: 0, gold: 1800 },
+            Resources { wood: 0, stone: 0, iron: 0, steel: 0, mana: 0, gold: 3200 },
+            Resources { wood: 0, stone: 0, iron: 0, steel: 0, mana: 0, gold: 5000 },
         ];
         self.mystic_well = [
-            Resources { wood: 0, stone: 0, iron: 0, food: 0, gold: 800 },
-            Resources { wood: 0, stone: 0, iron: 0, food: 0, gold: 1200 },
-            Resources { wood: 0, stone: 0, iron: 0, food: 0, gold: 2000 },
-            Resources { wood: 0, stone: 0, iron: 0, food: 0, gold: 3500 },
-            Resources { wood: 0, stone: 0, iron: 0, food: 0, gold: 5500 },
+            Resources { wood: 0, stone: 0, iron: 0, steel: 0, mana: 0, gold: 800 },
+            Resources { wood: 0, stone: 0, iron: 0, steel: 0, mana: 0, gold: 1200 },
+            Resources { wood: 0, stone: 0, iron: 0, steel: 0, mana: 0, gold: 2000 },
+            Resources { wood: 0, stone: 0, iron: 0, steel: 0, mana: 0, gold: 3500 },
+            Resources { wood: 0, stone: 0, iron: 0, steel: 0, mana: 0, gold: 5500 },
         ];
         self.watch_tower = [
-            Resources { wood: 0, stone: 0, iron: 0, food: 0, gold: 700 },
-            Resources { wood: 0, stone: 0, iron: 0, food: 0, gold: 1100 },
-            Resources { wood: 0, stone: 0, iron: 0, food: 0, gold: 1700 },
-            Resources { wood: 0, stone: 0, iron: 0, food: 0, gold: 2600 },
-            Resources { wood: 0, stone: 0, iron: 0, food: 0, gold: 4000 },
+            Resources { wood: 0, stone: 0, iron: 0, steel: 0, mana: 0, gold: 700 },
+            Resources { wood: 0, stone: 0, iron: 0, steel: 0, mana: 0, gold: 1100 },
+            Resources { wood: 0, stone: 0, iron: 0, steel: 0, mana: 0, gold: 1700 },
+            Resources { wood: 0, stone: 0, iron: 0, steel: 0, mana: 0, gold: 2600 },
+            Resources { wood: 0, stone: 0, iron: 0, steel: 0, mana: 0, gold: 4000 },
         ];
         self.castle = [
-            Resources { wood: 0, stone: 0, iron: 0, food: 0, gold: 1000 },
-            Resources { wood: 0, stone: 0, iron: 0, food: 0, gold: 2000 },
-            Resources { wood: 0, stone: 0, iron: 0, food: 0, gold: 4000 },
-            Resources { wood: 0, stone: 0, iron: 0, food: 0, gold: 8000 },
-            Resources { wood: 0, stone: 0, iron: 0, food: 0, gold: 16000 },
+            Resources { wood: 0, stone: 0, iron: 0, steel: 0, mana: 0, gold: 1000 },
+            Resources { wood: 0, stone: 0, iron: 0, steel: 0, mana: 0, gold: 2000 },
+            Resources { wood: 0, stone: 0, iron: 0, steel: 0, mana: 0, gold: 4000 },
+            Resources { wood: 0, stone: 0, iron: 0, steel: 0, mana: 0, gold: 8000 },
+            Resources { wood: 0, stone: 0, iron: 0, steel: 0, mana: 0, gold: 16000 },
         ];
 
         Ok(())
